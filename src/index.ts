09594a968d7510864c7b7f27f9ab5fc3cf7f3de1
Caio Victor <caiovictor.pcb@gmail.com>
2021-11-11 12:44:15 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+
+import Router from "./routes";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+
+app.use(Router);
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Rodando em http://localhost:5000")
+  })
